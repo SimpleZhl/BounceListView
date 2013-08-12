b98293457a12ad9b3aa11015c0a1e62235c6022a
u1aryz <u1aryz.d@gmail.com>
2013-08-12 23:29:13 +0900

    Use BounceListView in Activity

--- src/com/u1aryz/products/lib/MainActivity.java
+++ src/com/u1aryz/products/lib/MainActivity.java
@@ -3,13 +3,26 @@ package com.u1aryz.products.lib;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+import com.u1aryz.products.lib.view.BounceListView;
 
 public class MainActivity extends Activity {
+
+    BounceListView listView;
+
+    static final String[] SAMPLE_DATAS = new String[] { "a", "b", "c", "d",
+            "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+            "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (BounceListView) findViewById(R.id.list);
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, SAMPLE_DATAS));
     }
 
     @Override
