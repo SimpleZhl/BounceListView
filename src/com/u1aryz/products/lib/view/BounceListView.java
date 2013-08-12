4a3548533e708e380f2569325c4ccb370becbe0a
u1aryz <u1aryz.d@gmail.com>
2013-08-13 00:03:17 +0900

    Override the major lifecycle methods

--- src/com/u1aryz/products/lib/view/BounceListView.java
+++ src/com/u1aryz/products/lib/view/BounceListView.java
@@ -1,28 +1,80 @@
 package com.u1aryz.products.lib.view;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
+import android.widget.OverScroller;
 
-public class BounceListView extends AdapterView<ListAdapter> {
+/**
+ * A simple list view to bounce.
+ * 
+ * @author u1aryz
+ * 
+ */
+public class BounceListView extends AdapterView<ListAdapter> implements
+        OnTouchListener {
 
+    /** The overshoot the bounds of a scrolling operation. */
+    OverScroller mScroller;
+
+    /** The adapter with all the data. */
+    ListAdapter mAdapter;
+
+    /** Detects various gestures and events using the supplied MotionEvents. */
+    GestureDetector mGestureDetector;
+
+    /** A list of cached (re-usable) item views. */
+    List<View> mCachedItemViews = new LinkedList<View>();
+
+    /**
+     * Constructor.
+     * 
+     * @param context
+     */
     public BounceListView(Context context) {
         this(context, null);
     }
 
+    /**
+     * Constructor.
+     * 
+     * @param context
+     * @param attrs
+     */
     public BounceListView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
+    /**
+     * Constructor.
+     * 
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
     public BounceListView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init();
+    }
+
+    void init() {
+        mScroller = new OverScroller(getContext());
+        mGestureDetector = new GestureDetector(getContext(), mGestureListener);
+        setOnTouchListener(this);
     }
 
     @Override
     public ListAdapter getAdapter() {
-        return null;
+        return mAdapter;
     }
 
     @Override
@@ -32,10 +84,54 @@ public class BounceListView extends AdapterView<ListAdapter> {
 
     @Override
     public void setAdapter(ListAdapter adapter) {
+        mAdapter = adapter;
     }
 
     @Override
     public void setSelection(int position) {
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    /**
+     * A convenience class to extend when you only want to listen for a subset
+     * of all the gestures.
+     */
+    SimpleOnGestureListener mGestureListener = new SimpleOnGestureListener() {
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                float velocityY) {
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                float distanceX, float distanceY) {
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+    };
 }
